a2fb5040ba200d249bba7f2ba253fa1ffed99d6b
Amr Ahmed <amrahmedsaid179@gmail.com>
2025-08-25 11:01:04 +0300

    add_error_handeler_in _load_urls

--- src/app/featuresModules/projects/Ai-build-veba-Knowledge-Base/configes/config.service.ts
+++ src/app/featuresModules/projects/Ai-build-veba-Knowledge-Base/configes/config.service.ts
@@ -10,15 +10,19 @@ export class ConfigService {
   private config: any;
 
   constructor(private http: HttpClient) { }
-
   loadConfig(): Promise<any> {
-    return this.http.get(`${environment.URLS.BaseUrl}AiAgent/GetConfig`)
-      .toPromise()
-      .then(config => {
-        this.config = config;
-      });
+  return this.http.get(`${environment.URLS.BaseUrl}AiAgent/GetConfig`)
+    .toPromise()
+    .then(config => {
+      this.config = config;
+      return config;
+    })
+    .catch(err => {
+      console.error('Failed to load config:', err);
+      this.config = null; // fallback
+      return null; // or throw if you want to stop
+    });
   }
-
   get resourceMangmentUrl(): string {
     return this.config?.resorceUrl;
   }
